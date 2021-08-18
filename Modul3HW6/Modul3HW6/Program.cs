0050ab76cd5b77a935d2517d0733906f062dcf3e
Yurii <leoonovn@gmail.com>
2021-08-18 13:09:18 +0300

    Main

---
+++ Modul3HW6/Modul3HW6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Modul3HW6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviseProvider = new ServiceCollection()
+                .AddTransient<Starter>()
+                .BuildServiceProvider();
+
+            var starter = serviseProvider.GetService<Starter>();
+            starter.Run();
+        }
+    }
+}
